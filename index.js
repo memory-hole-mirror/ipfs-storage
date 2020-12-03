3c552f51def9807894f9f4ea69b65c1338e8bed5
Felix Fong <moongod101@hotmail.com>
2020-12-03 23:50:56 +0800

    add few route, one is for getting a signed url from S3

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 const AWS_SDK = require('aws-sdk');
 const UUID = require('node-uuid');
-const S3_remote_url = 'http://minio:9000'
 const fs = require('fs');
 const IPFS = require('ipfs-core');
 const util = require('util');
 const path = require('path');
 const { pipeline } = require('stream');
 const pump = util.promisify(pipeline);
+const mongoose = require('mongoose');
 
 const fastify = require('fastify')({
     logger: false,
 });
 fastify.register(require('fastify-multipart'));
 
+
+mongoose.connect('mongodb://mongo:27017/memoryhole', (err) => {
+    if(err) throw new Error(err);
+    console.info('Successfully connected to MongoDB');
+});
+
 const S3 = new AWS_SDK.S3({
-    endpoint: S3_remote_url,
+    endpoint: 'http://172.20.0.2:9000',
     accessKeyId: 'root',
     secretAccessKey: 'super_secret_key',
     s3ForcePathStyle: true,
-    signatureVersion: 'v4'
+    signatureVersion: 'v4',
 });
+
+const PostSchema = new mongoose.Schema({
+    ID: String,
+    time: {
+        create: Date,
+    },
+    ipfs: {
+        hash: {
+            cid: mongoose.Mixed,
+            cidString: String,
+        },
+        url: String,
+    },
+    s3: {
+        fileName: String,
+    }
+});
+
+const Posts = mongoose.model('Post', PostSchema);
 
 fastify.get('/', async (req, res) => {
     try {
@@ -28,6 +53,25 @@ fastify.get('/', async (req, res) => {
     } catch (err) {
         console.error(err);
         res.send('Fail to send the index.html file');
+    }
+});
+
+fastify.get('/posts', async (req, res) => {
+    try {
+
+        const posts = await Posts.find();
+
+        return res.type('application/json').send(posts);
+        
+    } catch (err) {
+        console.error(err);
+        return res.status(500).type('application/json').send(
+            JSON.stringify(
+                {
+                    error: 'internal error, /post route'
+                }
+            )
+        );
     }
 });
 
@@ -46,7 +90,6 @@ fastify.post('/upload', async (req, res) => {
         S3.upload(params, (err, data) => {
             if(err) throw new Error(err);
         });
-
         // Uploading file to the IPFS network 
         await pump(data.file, fs.createWriteStream(`/tmp/${newFileName}`));
         const ipfs = await IPFS.create();
@@ -56,21 +99,64 @@ fastify.post('/upload', async (req, res) => {
             file_name: newFileName,
             ipfs: {
                 hash: {
-                cid,
-                cid_string: cid.toString(),
-            },
+                    cid,
+                    cid_string: cid.toString(),
+                },
                 url: `https://ipfs.io/ipfs/${cid}`
             },
-            file: {
+            s3: {
                 name: fields.file.filename,
-                type: fields.file.mimetype
             },
         };
+
+        try {
+            const newPost = new Posts({
+                ID,
+                time: {
+                    create: new Date(),
+                },
+                ipfs: {
+                    hash: {
+                        cid,
+                        cidString: cid.toString(),
+                    },
+                    url: `https://ipfs.io/ipfs/${cid.toString()}`,
+                },
+                s3: {
+                    fileName: newFileName,
+                }
+            });
+            await newPost.save();
+        } catch (err) {
+            console.error(err);
+            return res.status(500).type('application/json').send( JSON.stringify({error: 'Internal Error, saving post to our database'}) );
+        }
+
         return res.status(201).type('application/json').send( JSON.stringify(output) );
     } catch (err) {
         console.error(err);
-        return res.status(500).send('Internal Error');
+        return res.status(500).type('application/json').send( JSON.stringify({error: 'Internal error, /upload route'}));
     }
+});
+
+
+fastify.get('/s3/get', (req, res) => {
+    const { file } = req.query;
+    if(!file) {
+        return res.status(400).type('application/json').send(JSON.stringify({error: 'Missing file param'}));
+    }
+    const params = {
+        Bucket: 'memory-hole',
+        Key: file,
+        Expires: 300, // Expire in 5 minutes
+    }
+    S3.getSignedUrl('getObject', params, (err, data) => {
+        if(err) {
+            console.error(err);
+            return res.status(500).type('application/json').send(JSON.stringify({error: 'Internal error, fail to obtain S3 file'}));
+        }
+        res.send(data)
+    });
 });
 
 fastify.listen(8000, '0.0.0.0', (err, addr) => {
